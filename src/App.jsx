9da9a4c1f84e3aa29821722f8e8fa7333ab95c99
DaviBisewski <davimbisewski@gmail.com>
2026-01-05 20:49:54 -0300

    Adiciona botão do WhatsApp e estilos correspondentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import WhatsAppButton from './components/ui/WhatsappButton'
 import Home from './pages/Home'
 
 function App() {
-  return <Home />
+  return (
+    <>
+      <Home />
+      <WhatsAppButton />
+    </>
+  );
 }
+
 
 export default App
---
+++ src/components/ui/WhatsappButton.jsx
@@ -0,0 +1,30 @@
+import zapVerde from "../../images/zapverde.png";
+import "./WhatsappButton.css";
+
+export default function WhatsAppButton() {
+  const phoneNumber = "5547984828184";
+
+  const message =
+    "Olá! Gostaria de fazer um pedido.";
+
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+
+  return (
+    <>
+      <a
+        href={whatsappUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Entrar em contato pelo WhatsApp"
+        title="Falar no WhatsApp"
+        className="whatsapp-fab"
+      >
+        <img src={zapVerde} alt="WhatsApp" />
+      </a>
+
+
+    </>
+  );
+}
