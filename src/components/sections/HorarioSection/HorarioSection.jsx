7b83820eab19bf9662f3f5c18c7a2a1b155366d8
DaviBisewski <davimbisewski@gmail.com>
2026-01-08 19:40:21 -0300

    Adiciona melhorias na seção de horário, incluindo ajustes de layout e responsividade, além de integrar a imagem do mascote.

--- src/components/sections/HorarioSection/HorarioSection.jsx
+++ src/components/sections/HorarioSection/HorarioSection.jsx
@@ -5,6 +5,7 @@ import CountdownFuncionamento from '../../layout/CountdownFuncionamento/Countdow
 
 import clockIcon from '../../../icons/relogio.png'
 import calendarIcon from '../../../icons/calendar.png'
+import Mascote from '../../../images/Mascote.png'
 
 export default function HorarioSection() {
   return (
@@ -21,19 +22,27 @@ export default function HorarioSection() {
         <div className="horario-card">
           <CountdownFuncionamento />
 
-          <div className="horario-row">
-            <img src={calendarIcon} alt="" aria-hidden />
-            <h3>Quarta a domingo e feriados</h3>
-          </div>
+          <div className="horario-main">
+            {/* esquerda */}
+            <div className="horario-col">
+              <img src={calendarIcon} alt="" aria-hidden />
+              <h3>Quarta a domingo e feriados</h3>
+            </div>
 
-          <div className="horario-row">
-            <img src={clockIcon} alt="" aria-hidden />
-            <h3 className="horario-hora">Das 10:00 às 13:30</h3>
-          </div>
+            {/* mascote */}
+            <img
+              src={Mascote}
+              alt=""
+              aria-hidden
+              className="horario-mascote"
+            />
 
-          <span className="horario-observacao">
-            Retirada de encomendas até 12:30
-          </span>
+            {/* direita */}
+            <div className="horario-col">
+              <img src={clockIcon} alt="" aria-hidden />
+              <h3>Das 10:00 às 13:30</h3>
+            </div>
+          </div>
         </div>
       </div>
     </section>
